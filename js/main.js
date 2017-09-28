12c0d9d371c725c48c3e7f1103d755a8c958a396
Stella Gordon-Zigel <stellagordon-zigel@Stellas-MacBook-Pro.local>
2017-09-28 13:12:30 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFound = "king";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne, cardTwo);
 console.log ("User flipped " + cardOne);
 console.log ("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
+
