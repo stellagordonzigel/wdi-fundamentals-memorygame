7e1f2cd5f6568c3fb0555ab32629f812181e0930
Stella <sgordonzigel@gmail.com>
2017-10-07 22:41:34 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -32,22 +32,32 @@ var checkForMatch = function () {
 	}
 }	
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 
-console.log("User flipped " + cards[cardId].rank);
-
-cardsInPlay.push(cards[cardId].rank);
-
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-
-	if (cardsInPlay.length === 2) {
-		checkForMatch ();
-	}
+		if (cardsInPlay.length === 2) {
+			checkForMatch ();
+		}
 };
 
-flipCard (0);
-flipCard (2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+
+};
+
+createBoard ();
+
 
 
 
